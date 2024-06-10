134db8d4964aec5124e84e091ac348dfcbce8bf5
cleyton <cleyton.p2021@gmail.com>
2024-06-10 16:33:22 -0300

    trabalho

---
+++ banco_restaurante/tb.sql
@@ -0,0 +1,7 @@
+use db_restaurante;
+select * from tb_cliente;
+select * from tb_mesa;
+select * from tb_tipo_prato;
+select * from tb_prato;
+select * from tb_situacao_pedido;
+select * from tb_pedido;
